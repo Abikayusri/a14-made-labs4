250e2b6a42b2c424e98f114595abb1f84cbf1ddd
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2019-04-23 14:31:23 +0700

    Add Room

---
+++ 07.Room/MyNoteApps/app/src/main/java/com/dicoding/picodiploma/mynoteapps/database/NoteDao.java
@@ -0,0 +1,30 @@
+package com.dicoding.picodiploma.mynoteapps.database;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Note note);
+
+    @Update()
+    void update(Note note);
+
+    @Delete()
+    void delete(Note note);
+
+    @Query("DELETE from note WHERE id = :idNote")
+    void delete(String idNote);
+
+    @Query("SELECT * from note ORDER BY id ASC")
+    LiveData<List<Note>> getAllNotes();
+}
---
+++ 07.Room/MyNoteApps/app/src/main/java/com/dicoding/picodiploma/mynoteapps/ui/main/MainViewModel.java
@@ -0,0 +1,26 @@
+package com.dicoding.picodiploma.mynoteapps.ui.main;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.dicoding.picodiploma.mynoteapps.database.Note;
+import com.dicoding.picodiploma.mynoteapps.repository.NoteRepository;
+
+import java.util.List;
+
+public class MainViewModel extends ViewModel {
+
+    private NoteRepository mNoteRepository;
+
+    private LiveData<List<Note>> mAllNotes;
+
+    public MainViewModel(Application application) {
+        mNoteRepository = new NoteRepository(application);
+        mAllNotes = mNoteRepository.getAllNotes();
+    }
+
+    LiveData<List<Note>> getAllNotes() {
+        return mAllNotes;
+    }
+}
